348691cb54ab9df6441aa63e27fc35378dd6b7be
Mingyu Li <mingyu.li.cn@gmail.com>
2015-06-23 16:26:57 +0800

    a

--- ks-install.sh
+++ ks-install.sh
@@ -14,9 +14,42 @@ fi
 
 
 sudo chown stack $LOG_DIR
-    # Set fd 1 and 2 to write the log file
-    exec 1> >( tools/outfilter.py -v -o "$LOG_DIR/ks-inst.log" ) 2>&1
+# Set fd 1 and 2 to write the log file
+exec 1> >( tools/outfilter.py -v -o "$LOG_DIR/ks-inst.log" ) 2>&1
 
+#Prerequisites
+sudo wget https://bootstrap.pypa.io/ez_setup.py --no-check-certificate -O - | python #这行会出一个错误，不过问题不大
+sudo apt-get install python-pip
+sudo apt-get install python-dev
+sudo apt-get install libffi-dev
+sudo apt-get install protobuf-compiler
+sudo apt-get install memcached
+sudo pip install pyeclib
+read -p "Prerequisites." var
+
+#Downloading kinetic-swift source
+cd ~
+git clone https://github.com/swiftstack/kinetic-swift.git
+cd kinetic-swift
+git submodule update --init
+read -p "Downloading kinetic-swift source." var
+
+#Installing from source
+sudo python setup.py develop
+cd swift
+sudo python setup.py develop
+cd ../python-swiftclient/
+sudo python setup.py install
+cd ../kinetic-py/
+git submodule init
+git submodule update
+sudo sh compile_proto.sh 
+sudo python setup.py develop
+cd ..
+read -p "Installing from source." var
+
+
+#Preparing the environment
 sudo mkdir $SWIFT_DIR
 sudo mkdir $SWIFT_DIR/sdv
 sudo mkdir /etc/swift
@@ -27,3 +60,12 @@ sudo cp $KS_SOURCE_DIR/swift/etc/proxy-server.conf-sample /etc/swift/proxy-serve
 sudo cp $KS_SOURCE_DIR/swift/etc/swift.conf-sample /etc/swift/swift.conf
 sudo mkdir /var/run/swift
 sudo mkdir /var/cache/swift
+read -p "Preparing the environment." var
+
+#Give the user that will be running swift read and write permission on the directories:
+sudo chmod o+rw $SWIFT_DIR
+sudo chmod o+rw $SWIFT_DIR/sdv
+sudo chmod o+rw /etc/swift
+sudo chmod o+rw /var/run/swift
+sudo chmod o+rw /var/cache/swift
+read -p "Give the user that will be running swift read and write permission on the directories." var
